e033245259f2302c616520ec3e35257a038d86c2
root <1544445921@qq.com>
2020-12-20 18:09:33 +0800

    OrdersController第二次提交的代码

--- src/main/java/com/offcn/controller/OrdersController.java
+++ src/main/java/com/offcn/controller/OrdersController.java
@@ -1,8 +1,13 @@
 package com.offcn.controller;
 
+import java.sql.SQLOutput;
+
 /**
  * @author guozhen
  * @create 2020-12-20  18:06
  */
 public class OrdersController {
+    public static void main(String[] args) {
+        System.out.println();
+    }
 }
